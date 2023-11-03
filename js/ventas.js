bfed939941cd97bcde7c15d9813992e573d5e866
Joshua Nuñez <yadidjoshua@gmail.com>
2023-11-02 21:40:58 -0300

    arreglos

--- js/ventas.js
+++ js/ventas.js
@@ -1,4 +1,4 @@
-// Función para cargar años en el select
+// Funciones de carga de datos
 function cargarAnios() {
   console.log("cargar anios");
   const anioSelect = document.getElementById("anio");
@@ -39,6 +39,97 @@ function cargarModelos(marca) {
         modeloSelect.appendChild(option);
       });
     });
+}
+
+function cargarStatus() {
+  console.log("cargar estados");
+  const estadoSelect = document.getElementById("estado");
+  const estados = [
+    { id: 0, nombre: "Usado" },
+    { id: 1, nombre: "Nuevo" },
+  ];
+  estados.forEach((estado) => {
+    const option = document.createElement("option");
+    option.value = estado.id;
+    option.textContent = estado.nombre;
+    estadoSelect.appendChild(option);
+  });
+}
+
+function cargarRatings() {
+  console.log("cargar ratings");
+  const ratingSelect = document.getElementById("rating");
+  const defaultOption = document.createElement("option");
+  defaultOption.textContent = "Seleccionar...";
+  ratingSelect.appendChild(defaultOption);
+
+  for (let i = 1; i <= 5; i++) {
+    const option = document.createElement("option");
+    option.value = i;
+    option.innerHTML = getRatingStarts(i);
+    ratingSelect.appendChild(option);
+  }
+}
+function cargarAutosIniciales() {
+  fetch("https://ha-front-api-proyecto-final.vercel.app/cars")
+    .then((response) => response.json())
+    .then((data) => {
+      mostrarAutos(data);
+    });
+}
+
+// Funciones de utilidad
+function getRatingStarts(rating) {
+  let stars = "";
+  for (let i = 0; i < rating; i++) {
+    stars += "⭐";
+  }
+  return stars;
+}
+function formatPrice(price) {
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+function mostrarAutos(autos) {
+  const contenedorAutos = document.querySelector(".cards-list");
+  contenedorAutos.innerHTML = "";
+
+  autos.forEach((auto) => {
+    const card = document.createElement("div");
+    card.className = "card mycard mb-4";
+    const stars = getRatingStarts(auto.rating);
+    const statusColor = auto.status === 1 ? "orange" : "gray";
+    const formattedPrice = formatPrice(auto.price_usd);
+    const cardContent = `
+          <div class="row">
+            <div class="col-xl-4 col-md-12 position-relative">
+              <img src="${auto.image}" class="card-img h-100 " alt="${
+      auto.model
+    }" style="background-size: cover" />
+              <button class="btn position-absolute top-0 t-white start-0" style="background-color: ${statusColor}; color: white;">${
+      auto.status === 1 ? "Nuevo" : "Usado"
+    }</button>
+            </div>
+            <div class="col-xl-8">
+              <div class="card-body fs-6 custom-padding">
+                <div class="d-flex justify-content-between align-items-center">
+                  <h5 class="card-title mb-2">${auto.brand} ${auto.model}</h5>
+                  <small class="text-muted">${
+                    auto.year
+                  } | USD ${formattedPrice} | ${stars}</small>
+                </div>
+                <p class="card-text">${auto.description}</p>
+                <button class="btn btn-success comprar"><i class="bi bi-cart-plus"></i> Comprar</button>
+                <button class="btn btn-light border-dark info"><i class="bi bi-plus-square"></i> Más información</button>
+                <button class="btn btn-light border-dark compartir"><i class="bi bi-share"></i> Compartir</button>
+              </div>
+            </div>
+          </div>
+        `;
+
+    card.innerHTML = cardContent;
+    contenedorAutos.appendChild(card);
+  });
 }
 
 function filtrarAutos() {
@@ -86,81 +177,6 @@ function filtrarAutos() {
     });
 }
 
-function cargarStatus() {
-  console.log("cargar estados");
-  const estadoSelect = document.getElementById("estado");
-  const estados = [
-    { id: 0, nombre: "Usado" },
-    { id: 1, nombre: "Nuevo" },
-  ];
-  estados.forEach((estado) => {
-    const option = document.createElement("option");
-    option.value = estado.id;
-    option.textContent = estado.nombre;
-    estadoSelect.appendChild(option);
-  });
-}
-
-function cargarRatings() {
-  console.log("cargar ratings");
-  const ratingSelect = document.getElementById("rating");
-  const defaultOption = document.createElement("option");
-  defaultOption.textContent = "Seleccionar...";
-  ratingSelect.appendChild(defaultOption);
-
-  for (let i = 1; i <= 5; i++) {
-    const option = document.createElement("option");
-    option.value = i;
-    option.textContent = `${i} Estrella${i > 1 ? "s" : ""}`;
-    ratingSelect.appendChild(option);
-  }
-}
-
-function getRatingStarts(rating) {
-  let stars = "";
-  for (let i = 0; i < rating; i++) {
-    stars += '<i class="bi bi-star-fill" style="color: gold;"></i>';
-  }
-  return stars;
-}
-
-function mostrarAutos(autos) {
-  const contenedorAutos = document.querySelector(".cards-list");
-  contenedorAutos.innerHTML = "";
-
-  autos.forEach((auto) => {
-    const card = document.createElement("div");
-    card.className = "card mycard mb-4";
-    const stars = getRatingStarts(auto.rating);
-    const cardContent = `
-          <div class="row">
-            <div class="col-xl-4 col-md-12">
-              <img src="${auto.image}" class="card-img h-100 " alt="${
-      auto.model
-    }" style="background-size: cover" />
-            </div>
-            <div class="col-xl-8">
-              <div class="card-body fs-6 custom-padding">
-                <div class="d-flex justify-content-between align-items-center">
-                  <h5 class="card-title mb-2">${auto.brand} ${auto.model}</h5>
-                  <small class="text-muted">${auto.year} | USD ${
-      auto.price_usd
-    } | ${auto.status === 1 ? "Nuevo" : "Usado"} |${stars}</small>
-                </div>
-                <p class="card-text">${auto.description}</p>
-                <button class="btn btn-success comprar">Comprar</button>
-                <button class="btn btn-light border-dark info">Más información</button>
-                <button class="btn btn-light border-dark compartir">Compartir</button>
-              </div>
-            </div>
-          </div>
-        `;
-
-    card.innerHTML = cardContent;
-    contenedorAutos.appendChild(card);
-  });
-}
-
 // Event listeners
 document.getElementById("marca").addEventListener("change", function () {
   console.log("cargar modelos");
@@ -180,3 +196,4 @@ cargarAnios();
 cargarMarcas();
 cargarStatus();
 cargarRatings();
+cargarAutosIniciales();
